2ae4dc08086fbf8f6c4d182399f438f877db7136
iamyewon <101702107+iamyewon@users.noreply.github.com>
2022-07-12 15:47:17 +0900

    7.12
    middle

---
+++ te_House.java
@@ -0,0 +1,15 @@
+
+public class House {
+	String mete;
+	int floor;
+	public House() {
+	
+	}
+	public House(String mete) {
+		this.mete=mete;
+	}
+	public House(String mete, int floor) {
+		this.mete = mete;
+		this.floor = floor;
+	}
+}
